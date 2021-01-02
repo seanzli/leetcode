a0111d10585bde9ccea6311bcbdfa489c74d94bd
seanzli <seanzli@outlook.com>
2021-01-02 23:12:03 +0800

    code11 container with most water

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,14 @@
 
-#include "../code9/solution.hpp"
+#include "../code11/solution.hpp"
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
     //string s = "-91283472332";//argv[1];
-    int num = 1234567899;//atoi(argv[1]);
-    //cout << test->isPalindrome(num) << endl;
-    if (test->isPalindrome(num))
-        cout<< "true" << endl;
-    else
-        cout<< "false" << endl;
-    
+    vector<int> height = {1,8,6,2,5,4,8,3,7};//atoi(argv[1]);
+
+    cout <<test->maxArea(height)<<endl;
+
     return 0;
 }

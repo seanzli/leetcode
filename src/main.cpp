8e34f5295f3612dd183008c4a78c0cd78e8af7e9
seanzli <seanzli@outlook.com>
2021-01-03 20:59:12 +0800

    code18 4sum

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 
-#include "../code17/solution.hpp"
+#include "../code18/solution.hpp"
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
     //vector<string> strs = {"reflower","flow","flight"};
-    string str = "2";
-    test->letterCombinations(str);
+    //string str = "2";
+    vector<int> nums= {1,0,-1,0,-2,2};
+    int target = 0;
+    test->fourSum(nums, target);
 
     return 0;
 }

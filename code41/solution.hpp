be09b133dcc6ab275706d40a1565e161d9ef0651
seanzli <seanzli@outlook.com>
2021-01-06 17:22:43 +0800

    code41 first missing positive

--- code41/solution.hpp
+++ code41/solution.hpp
@@ -2,12 +2,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-
+        int cur = 1;
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i]<=0)
+                continue;
+            hash[nums[i]]++;
+            if (cur == nums[i])
+            {
+                cur++;
+                while (hash.find(cur) != hash.end())
+                    cur++;
+            }
+        }
+        return cur;
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@ int main(int argc, char* argv[])
     start_time = clock();
     Solution *test = new Solution();
 
-    vector <int> nums = {1,2,0};
+    vector <int> nums = {2147483647,2147483646,2147483645,3,2,1,-1,0,-2147483648};
 
-    cout << test->firstMissingPositive(nums);
+    cout << test->firstMissingPositive(nums)<<endl;
 
 /*     for (int i = 0; i < out.size(); i++)
     {

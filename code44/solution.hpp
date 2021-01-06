d30b9b4641e4586d6143953e249ccf77dfc6cbc6
seanzli <seanzli@outlook.com>
2021-01-06 21:13:42 +0800

    code44 wildcard matching

---
+++ code44/solution.hpp
@@ -0,0 +1,89 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+/* class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size();
+        int n = p.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        dp[0][0] = true;
+        for (int i = 1; i <= n; ++i) {
+            if (p[i - 1] == '*') {
+                dp[0][i] = true;
+            }
+            else {
+                break;
+            }
+        }
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 1] | dp[i - 1][j];
+                }
+                else if (p[j - 1] == '?' || s[i - 1] == p[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}; */
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        auto allStars = [](const string& str, int left, int right) {
+            for (int i = left; i < right; ++i) {
+                if (str[i] != '*') {
+                    return false;
+                }
+            }
+            return true;
+        };
+        auto charMatch = [](char u, char v) {
+            return u == v || v == '?';
+        };
+
+        while (s.size() && p.size() && p.back() != '*') {
+            if (charMatch(s.back(), p.back())) {
+                s.pop_back();
+                p.pop_back();
+            }
+            else {
+                return false;
+            }
+        }
+        if (p.empty()) {
+            return s.empty();
+        }
+
+        int sIndex = 0, pIndex = 0;
+        int sRecord = -1, pRecord = -1;
+        while (sIndex < s.size() && pIndex < p.size()) {
+            if (p[pIndex] == '*') {
+                ++pIndex;
+                sRecord = sIndex;
+                pRecord = pIndex;
+            }
+            else if (charMatch(s[sIndex], p[pIndex])) {
+                ++sIndex;
+                ++pIndex;
+            }
+            else if (sRecord != -1 && sRecord + 1 < s.size()) {
+                ++sRecord;
+                sIndex = sRecord;
+                pIndex = pRecord;
+            }
+            else {
+                return false;
+            }
+        }
+        return allStars(p, pIndex, p.size());
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 
-#include "../code42/solution.hpp"
+#include "../code44/solution.hpp"
 #include <ctime>
 
 int main(int argc, char* argv[])
@@ -8,19 +8,10 @@ int main(int argc, char* argv[])
     start_time = clock();
     Solution *test = new Solution();
 
-    vector <int> nums = {0,1,0,2,1,0,1,3,2,1,2,1};
+    string s = "adceb";
+    string p = "a*c*b";
 
-    cout << test->trap(nums)<<endl;
-
-/*     for (int i = 0; i < out.size(); i++)
-    {
-        for (int j = 0; j < out[i].size(); j++)
-        {
-            cout << out[i][j] << ",";
-        }
-        cout << endl;        
-    } */
-    
+    cout << test->isMatch(s,p)<<endl;    
 
     cout << (double)(clock() - start_time)/CLOCKS_PER_SEC << endl;
 

1289bd4ec8eb0e532a3b421b2ce6a1d3bf583685
seanzli <seanzli@outlook.com>
2021-01-07 14:34:20 +0800

    code50 power(x,n)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 
-#include "../code49/solution.hpp"
+#include "../code50/solution.hpp"
 #include <ctime>
 
 int main(int argc, char* argv[])
@@ -9,16 +9,16 @@ int main(int argc, char* argv[])
     Solution *test = new Solution();
 
     vector<string> strs = {"tea","and","ace","ad","eat","dans"};
-    vector<vector<string>> out = test->groupAnagrams(strs);
+    cout << test->myPow(2 , 10) << endl;
 
-    for (int i = 0; i < out.size(); i++)
+/*     for (int i = 0; i < out.size(); i++)
     {
         for (int j = 0; j < out[i].size(); j++)
         {
             cout << out[i][j] << ",";
         }
         cout << endl;
-    }
+    } */
     
     cout << (double)(clock() - start_time)/CLOCKS_PER_SEC << endl;
 

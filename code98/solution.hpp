73c9fdeb2636531b52a1221cb077f2206742641f
seanzli <seanzli@outlook.com>
2021-01-13 16:58:57 +0800

    code98 validate binary search tree

---
+++ code98/solution.hpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <limits.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool helper(TreeNode* root, long long lower, long long upper) {
+        if (root == nullptr) {
+            return true;
+        }
+        if (root -> val <= lower || root -> val >= upper) {
+            return false;
+        }
+        return helper(root -> left, lower, root -> val) && helper(root -> right, root -> val, upper);
+    }
+    bool isValidBST(TreeNode* root) {
+        return helper(root, LONG_MIN, LONG_MAX);
+    }
+};

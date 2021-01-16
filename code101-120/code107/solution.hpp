7efbb62ad5bdcebe36b7b38a90321df51810280a
seanzli <seanzli@outlook.com>
2021-01-16 17:41:49 +0800

    code107 binary tree level order traversal II

---
+++ code101-120/code107/solution.hpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> out;
+    vector<TreeNode*> list;
+    vector<int> cur;
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        if (root == nullptr)
+            return out;
+        list.push_back(root);
+        getListValue();
+        reverse(out.begin(), out.end());
+        return out;
+    }
+    void getListValue(){
+        vector<int> cur;
+        vector<TreeNode* >cur_list;
+        if (list.size()<1)
+            return;
+        
+        for (int i = 0; i < list.size(); i++)
+        {
+            cur.push_back(list[i]->val);
+            if (list[i]->left)
+                cur_list.push_back(list[i]->left);
+            if (list[i]->right)
+                cur_list.push_back(list[i]->right);
+        }
+        out.push_back(cur);
+        list = cur_list;
+        getListValue();
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
 
-#include "../code105/solution.hpp"
+#include "../code107/solution.hpp"
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
-    vector<int> preorder = {3,9,20,15,7};
-    vector<int> inorder = {9,3,15,20,7};
-    TreeNode *in = test->buildTree(preorder, inorder);
+    TreeNode *in = new TreeNode(3);
+    in->left = new TreeNode(9);
+    in->left->left = new TreeNode(1);
+    in->left->right = new TreeNode(2);
+    in->right = new TreeNode(20);
+    in->right->left = new TreeNode(15);
+    in->right->right = new TreeNode(7);
+    in->right->right->left = new TreeNode(7);
+    vector<vector<int>> out =  test->levelOrderBottom(in);
 
-    
-
+    for (int i = 0; i < out.size(); i++)
+    {
+        for (int j = 0; j < out[i].size(); j++)
+        {
+            cout << out[i][j] << ",";
+        }
+        cout << endl;
+    }
     return 0;
 }

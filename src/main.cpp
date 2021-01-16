c5cbd1bcc8f4721dd1edbd018b5deb35dafd9025
seanzli <seanzli@outlook.com>
2021-01-16 17:16:32 +0800

    code105 construct binary tree

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,15 @@
 
-#include "../code104/solution.hpp"
+#include "../code105/solution.hpp"
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
-    TreeNode *in = new TreeNode(3);
-    in->left = new TreeNode(9);
-    in->left->left = new TreeNode(1);
-    in->left->right = new TreeNode(2);
-    in->right = new TreeNode(20);
-    in->right->left = new TreeNode(15);
-    in->right->right = new TreeNode(7);
-    in->right->right->left = new TreeNode(7);
-    cout <<  test->maxDepth(in) << endl;
+    vector<int> preorder = {3,9,20,15,7};
+    vector<int> inorder = {9,3,15,20,7};
+    TreeNode *in = test->buildTree(preorder, inorder);
 
-/*     for (int i = 0; i < out.size(); i++)
-    {
-        for (int j = 0; j < out[i].size(); j++)
-        {
-            cout << out[i][j] << ",";
-        }
-        cout << endl;
-    } */
+    
+
     return 0;
 }

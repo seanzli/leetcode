b15ca2bf7ef7bd99d73c2b296dc6a4d358663a8c
seanzli <seanzli@outlook.com>
2021-01-19 21:00:37 +0800

    code169 majority element

--- code168/solution.hpp
+++ code168/solution.hpp
@@ -22,3 +22,21 @@ public:
         return out;
     }
 };
+
+//code169
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = -1;
+        int count = 0;
+        for (int num : nums) {
+            if (num == candidate)
+                ++count;
+            else if (--count < 0) {
+                candidate = num;
+                count = 1;
+            }
+        }
+        return candidate;
+    }
+};

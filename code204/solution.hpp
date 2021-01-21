5d09a1fa636f0d2bf1ebc1b996b0770b34ed140c
seanzli <seanzli@outlook.com>
2021-01-21 12:50:00 +0800

    code204 count primes

---
+++ code204/solution.hpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <regex>
+#include <math.h>
+#include <queue>
+#include <limits.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<int> isPrime(n, 1);
+        int ans = 0;
+        for (int i = 2; i < n; ++i) {
+            if (isPrime[i]) {
+                ans += 1;
+                if ((long long)i * i < n) {
+                    for (int j = i * i; j < n; j += i) {
+                        isPrime[j] = 0;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};

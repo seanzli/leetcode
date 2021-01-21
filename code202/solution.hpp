ca3bd6a990948da1f2cde4906dbefb9991490305
seanzli <seanzli@outlook.com>
2021-01-21 11:30:40 +0800

    code 202 happy number

---
+++ code202/solution.hpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <regex>
+#include <math.h>
+#include <queue>
+#include <limits.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int bitSquareSum(int n) {
+        int sum = 0;
+        while(n > 0)
+        {
+            int bit = n % 10;
+            sum += bit * bit;
+            n = n / 10;
+        }
+        return sum;
+    }
+    
+    bool isHappy(int n) {
+        int slow = n, fast = n;
+        do{
+            slow = bitSquareSum(slow);
+            fast = bitSquareSum(fast);
+            fast = bitSquareSum(fast);
+        }while(slow != fast);
+        
+        return slow == 1;
+    }
+};

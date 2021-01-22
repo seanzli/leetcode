d5f51cbf78f3f38e352323eac0e4d72132f01a8c
seanzli <seanzli@outlook.com>
2021-01-22 21:03:56 +0800

    code213 house robber II

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 
-#include "../code209/solution.hpp"
+#include "../code213/solution.hpp"
 
 
 
@@ -7,15 +7,8 @@ int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
-    vector<vector<char>> grid = {
-        {'1','1','0','1','0'},
-        {'1','1','0','1','0'},
-        {'1','1','0','0','0'},
-        {'0','0','0','0','0'},
-    };
-
-    vector<int> in = {2,3,1,2,4,3};
-    cout << test->minSubArrayLen(7,in) << endl;
+    vector<int> in = {2,3,2};
+    cout << test->rob(in) << endl;
 
     /* while (in){
         cout << in->val << "->";

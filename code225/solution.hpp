0fce77ea2db25cb292631a3004fb14059426cf2a
seanzli <seanzli@outlook.com>
2021-01-25 17:40:28 +0800

    code225 implement stack using queues

---
+++ code225/solution.hpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class MyStack {
+private:
+    queue<int> q;
+public:
+    /** Initialize your data structure here. */
+    MyStack() {
+
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        q.push(x);
+        int n = q.size();
+        for (int i = 0; i<n-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int cur = q.front();
+        q.pop();
+        return cur;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return q.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q.empty();
+    }
+};

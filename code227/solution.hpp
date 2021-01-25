98ee2f7c85a5c54bcbd3bb05641d7994dcfe1253
seanzli <seanzli@outlook.com>
2021-01-25 21:17:16 +0800

    code227 basic calculator II

---
+++ code227/solution.hpp
@@ -0,0 +1,83 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+private:
+
+public:
+    int calculate(string s) {
+        int i = 0;
+        return calcS(s, i);
+    }
+
+    int calcS(string &s, int &i){
+        vector<int> nums;
+        vector<char> opt;
+        int out = 0, cur=0, flag=0, signal=1;
+        for (; i < s.size(); i++)
+        {
+            if ((s[i]>='0')&&(s[i]<='9'))
+                cur = cur*10-'0'+s[i];
+            else if ((s[i]=='+')||(s[i]=='-')){
+                if (flag==1)
+                    cur = calc(cur, nums, opt);
+                nums.push_back(cur);
+                cur=0;
+                opt.push_back(s[i]);
+                flag=0;
+            }
+            else if ((s[i] == '*')||(s[i]=='/')){
+                if (flag==1)
+                    cur = calc(cur, nums, opt);
+                nums.push_back(cur);
+                opt.push_back(s[i]);
+                flag=1;
+                cur=0;
+            }
+            else if (s[i]=='('){
+                i++;
+                cur = calcS(s, i);
+                if (flag==1)
+                    cur = calc(cur, nums, opt);
+                flag=0;
+            }
+            else if (s[i]==')')
+                break;
+            else if (s[i]==' ')
+                continue;
+        }
+        if (flag==1)
+            cur = calc(cur, nums, opt);
+        nums.push_back(cur);
+        return calcStack(nums, opt);
+    }
+    int calc(int cur, vector<int> &nums, vector<char> &opt){
+        int pre = nums.back();nums.pop_back();
+        if (opt.back() == '*'){
+            opt.pop_back();
+            return (pre*cur);
+        }
+        else{
+            opt.pop_back();
+            return (pre/cur);
+        }
+    }
+
+    int calcStack(vector<int> &nums, vector<char> &opt){
+        if (!nums.size())
+            return 0;
+        int out = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (opt[i-1]=='+')
+                out += nums[i];
+            else
+                out -= nums[i];
+        }
+        return out;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 
-#include "../code226/solution.hpp"
+#include "../code227/solution.hpp"
 
 
 
@@ -7,14 +7,8 @@ int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
-    TreeNode *root = new TreeNode(4);
-    root->left = new TreeNode(2);
-    root->left->left = new TreeNode(1);
-    root->left->right = new TreeNode(3);
-    root->right = new TreeNode(7);
-    root->right->left = new TreeNode(6);
-    root->right->right = new TreeNode(9);
-    TreeNode *out =  test->invertTree(root);
+    string s = "(3+5) / 2";
+    cout << test->calculate(s) << endl;
 
     /* while (in){
         cout << in->val << "->";

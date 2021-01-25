9dab5bcf56f585f80d32b2c60cda12b9c8f5bf54
seanzli <seanzli@outlook.com>
2021-01-25 21:36:33 +0800

    code229 majority element II

---
+++ code229/solution.hpp
@@ -0,0 +1,64 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int len = nums.size();
+        vector<int>res, cands, cnts;
+        if(len == 0){//没有元素，直接返回空数组
+            return res;
+        }
+        cands.assign(2, nums[0]);
+        cnts.assign(2, 0);
+        //第1阶段 成对抵销
+        for(auto num: nums){
+            bool flag = false;
+            for(int i = 0; i < cands.size(); ++i){
+                if(num == cands[i]){
+                    ++cnts[i];
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                bool flag2 = false;
+                for(int j = 0; j < cands.size(); ++j){
+                    if(cnts[j] == 0){
+                        flag2 = true;
+                        cands[j] = num;
+                        cnts[j]++;
+                    }
+                }
+                if(!flag2){
+                    for(int j = 0; j < cnts.size(); ++j){
+                        --cnts[j];
+                    }
+                }
+            }
+        }
+
+        //第2阶段 计数 数目要超过三分之一
+        cnts[0] = cnts[1] = 0;
+        if(cands[0] == cands[1])
+            cands.pop_back();
+        for(auto num:nums){
+            for(int i = 0; i < cands.size(); ++i){
+                if(cands[i] == num){
+                    ++cnts[i];
+                    break;
+                }
+            }
+        }
+        for(int i = 0; i < cands.size(); ++i){
+            if(cnts[i] > len / 3){
+                res.push_back(cands[i]);
+            }
+        }
+        return res;
+    }
+};

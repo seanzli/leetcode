1376fac2c0fdf4ca57db82f81f7112d84a8bcdb8
seanzli <seanzli@outlook.com>
2021-01-27 10:22:16 +0800

    code241

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
     Solution *test = new Solution();
     vector<int> in = {1,3,-1,-3,5,3,6,7};
 
-    vector<int> out = test->maxSlidingWindow(in,3);
+    vector<int> out = test->diffWaysToCompute("2-1-1");
 
     /* while (in){
         cout << in->val << "->";

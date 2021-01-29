6e5e92165b6caa40102f47d27bf75f1c9705454a
seanzli <seanzli@outlook.com>
2021-01-29 21:50:38 +0800

    code701

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 
-#include "../code/offer_46/solution.hpp"
+#include "../code701/solution.hpp"
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
+    TreeNode* in = new TreeNode(4);
+    in->left = new TreeNode(2);
+    in->right = new TreeNode(7);
+    in->left->right = new TreeNode(3);
+    in->left->left = new TreeNode(1);
 
-    cout <<  test->translateNum(12258) << endl;
+    test->insertIntoBST(in,5);
 
 
     return 0;

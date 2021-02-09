f1e4200541aee92024d69a1710357a1272434650
seanzli <seanzli@outlook.com>
2021-02-09 11:13:57 +0800

    offer27

---
+++ code/offer_27/solution.hpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <math.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        if (root == nullptr)    return;
+        TreeNode* cur = new TreeNode(root->val);
+        if (root->left)
+            cur->right = mirrorTree(root->left);
+        if (root->right)
+            cur->left = mirrorTree(root->right);
+        return cur;
+    }
+};

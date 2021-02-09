c14b293868218d4988b5cec9f47d413b02067f10
seanzli <seanzli@outlook.com>
2021-02-09 11:27:35 +0800

    code17_10

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 
-#include "../code992/solution.hpp"
+#include "../code/code17_10/solution.hpp"
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
-    vector<int> in = {1,2,1,2,3};
-    test->subarraysWithKDistinct(in,2);
+    vector<int> in = {1,2,5,9,5,9,5,5,5};
+    test->majorityElement(in);
 
     return 0;
 }

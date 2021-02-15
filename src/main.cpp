883e9342fd4f93abf98b66688385ecdf391cc2aa
seanzli <seanzli@outlook.com>
2021-02-15 11:30:13 +0800

    code1071

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 
-#include "../code718/solution.hpp"
+#include "../code1071/solution.hpp"
 
 #include <queue>
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
-    
-    vector<int> A = {1,2,3,2,1};
-    vector<int> B = {3,2,1,4,7};
-    cout<<test->findLength(A,B) << endl;
+    string a = "ABCABC";
+    string b = "ABC";
+    cout<<test->gcdOfStrings(a,b) << endl;
     return 0;
 }

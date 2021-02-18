c275edf28ebecd8c3d67c07b3a9a546245d4921b
seanzli <seanzli@outlook.com>
2021-02-18 09:38:00 +0800

    code807

---
+++ code807/solution.hpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        int row = grid.size(), col = grid[0].size();
+        vector<int> row_max(row ,0);
+        vector<int> col_max(col, 0);
+        for (int i = 0; i< row; i++){
+            row_max[i] = *max_element(grid[i].begin(), grid[i].end());
+            for (int j=0; j<col; j++){
+                col_max[j] = max(col_max[j], grid[i][j]);
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < col; j++){
+                sum += (min(row_max[i], col_max[j]) - grid[i][j]);
+            }
+        }
+
+        return sum;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 
-#include "../code566/solution.hpp"
+#include "../code787/solution.hpp"
 
 #include <queue>
 
@@ -7,8 +7,8 @@ int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
-    vector<vector<int>> in = {{1,2},{3,4}};
-    test->matrixReshape(in, 4, 1);
+    vector<vector<int>> flights = {{0,1,1},{0,2,5},{1,2,1},{2,3,1},{3,4,1}};
+    cout << test->findCheapestPrice(5, flights, 0, 4, 2) << endl;
     
     return 0;
 }

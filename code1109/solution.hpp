b2bd8de094296e7a641d4e4243247e20ef35e586
seanzli <seanzli@outlook.com>
2021-02-22 21:42:59 +0800

    code1109

---
+++ code1109/solution.hpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> res(n, 0);
+        vector<int> diff(n + 1, 0);
+        for (vector<int> cur : bookings) {
+            diff[cur[0] - 1] += cur[2];
+            diff[cur[1]] -= cur[2];
+        }
+
+        res[0] = diff[0];
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] + diff[i];
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> res(n, 0);
+        for (vector<int> cur : bookings) {
+            res[cur[0] - 1] += cur[2];
+            if (cur[1] < n)
+                res[cur[1]] -= cur[2];
+        }
+
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] + res[i];
+        }
+        return res;
+    }
+};

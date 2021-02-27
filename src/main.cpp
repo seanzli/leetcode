437cc89d8ebd9700b6ecf478cd57873dd2cb732c
seanzli <seanzli@outlook.com>
2021-02-27 11:35:38 +0800

    code1539

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,10 @@
 
-#include "../code1373/solution.hpp"
+#include "../code1025/solution.hpp"
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
-    TreeNode* in = new TreeNode(3);
-    in->left = new TreeNode(4);
-    in->left->left = new TreeNode(2);
-    in->left->right = new TreeNode(4);
-    in->right = new TreeNode(3);
-    in->right->left = new TreeNode(2);
-    in->right->right = new TreeNode(5);
-    in->right->right->left = new TreeNode(4);
-    in->right->right->right = new TreeNode(6);
 
-    test->maxSumBST(in);
+    cout << test->divisorGame(1000) << endl;
     return 0;
 }

8526595b2369eaf3900afb6927e74f9257eadace
seanzli <seanzli@outlook.com>
2021-03-02 17:52:52 +0800

    code854

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 
-#include "../code/offer_ 67/solution.hpp"
+#include "../code854/solution.hpp"
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
-    string s = "-91283472332";
-    cout << test->strToInt(s) << endl;
+    string a = "aabbccddee", b = "dcacbedbae";
+    test->kSimilarity(a,b);
     return 0;
 }

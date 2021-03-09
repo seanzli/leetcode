116ef06db163fa6e527452d91e01db772837b78c
seanzli <seanzli@outlook.com>
2021-03-09 18:20:35 +0800

    code1325

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 
-#include "../code453/solution.hpp"
+#include "../code1325/solution.hpp"
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
-    vector<int> in = {1,2,3};
-    test->minMoves(in);
+    TreeNode* in = new TreeNode(1);
+    in->left = new TreeNode(2);
+    in->left->left = new TreeNode(2);
+    in->right = new TreeNode(3);
+    in->right->left = new TreeNode(2);
+    in->right->right = new TreeNode(4);
+    test->removeLeafNodes(in, 2);
     return 0;
 }

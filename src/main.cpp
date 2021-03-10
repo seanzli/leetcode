0a884bddde95c2456d7a4d5100c129ca203e01de
seanzli <seanzli@outlook.com>
2021-03-10 09:32:54 +0800

    code224

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,10 @@
 
-#include "../code1325/solution.hpp"
+#include "../code221-240/code224/solution.hpp"
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
-    TreeNode* in = new TreeNode(1);
-    in->left = new TreeNode(2);
-    in->left->left = new TreeNode(2);
-    in->right = new TreeNode(3);
-    in->right->left = new TreeNode(2);
-    in->right->right = new TreeNode(4);
-    test->removeLeafNodes(in, 2);
+    test->calculate("-(1+(4+5+2)-3)-(6+8)");
     return 0;
 }

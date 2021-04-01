347e7377dcbd58329607f4e7552d29c8b710ba10
seanzli <seanzli@outlook.com>
2021-04-01 13:12:45 +0800

    code239

--- code221-240/code239/solution.hpp
+++ code221-240/code239/solution.hpp
@@ -2,10 +2,11 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <queue>
 
 using namespace std;
 
-class Solution {
+/* class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
@@ -29,5 +30,50 @@ public:
             ans.push_back(nums[q.front()]);
         }
         return ans;
+    }
+}; */
+
+/* class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> que; // idx num; large -> small
+        vector<int> res;
+        for (int i = 0; i < k; i++) {
+            while (que.size() > 0 && nums[que.back()] < nums[i]) {
+                que.pop_back();
+            } 
+            que.push_back(i);
+        }
+        res.push_back(nums[que.front()]);
+        for (int i = k; i < nums.size(); i++) {
+            while (que.size() > 0 && nums[que.back()] < nums[i])
+                que.pop_back();
+            que.push_back(i);
+            while (i - que.front() >= k)
+                que.pop_front();
+            res.push_back(nums[que.front()]);
+        }
+        return res;
+    }
+}; */
+
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> que;
+        for (int i = 0; i < k - 1; i++) {
+            que.push(make_pair(nums[i], i));
+        }
+
+        vector<int> res;
+        for (int i = k - 1; i < nums.size(); i++) {
+            que.push(make_pair(nums[i], i));
+            while (i - que.top().second >= k) {
+                que.pop();
+            }
+            res.push_back(que.top().first);
+        }
+        return res;
     }
 };

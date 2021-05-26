8b088aaf423d56256cedb747908f549e889cb6f0
seanzli <seanzli@outlook.com>
2021-05-26 09:34:01 +0800

    code 1190

--- code1190/solution.hpp
+++ code1190/solution.hpp
@@ -3,10 +3,11 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <stack>
 
 using namespace std;
 
-class Solution {
+/* class Solution {
 public:
     string reverseParentheses(string s) {
         int idx = 0;
@@ -25,6 +26,40 @@ public:
             } else {
                 res += s[idx];
             }
+        }
+        return res;
+    }
+}; */
+
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        string res = "";
+        int n = s.size();
+        vector<int> pair(n);
+        stack<int> stk;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                 stk.push(i);
+            }
+            else if (s[i] == ')') {
+                pair[stk.top()] = i;
+                pair[i] = stk.top();
+                stk.pop();
+            } else {
+                continue;
+            }
+        }
+
+        int idx = 0, step = 1;
+        while (idx < n) {
+            if (s[idx] == '(' || s[idx] == ')') {
+                idx = pair[idx];
+                step = -step;
+            } else {
+                res.push_back(s[idx]);
+            }
+            idx += step;
         }
         return res;
     }

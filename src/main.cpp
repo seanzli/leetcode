61d6d6e7a4cdc0bc07df4cb1bab402dd367f869b
seanzli <76615961+seanzli@users.noreply.github.com>
2021-05-30 21:07:36 +0800

    code offer 37

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 
-#include "../code221-240/code231/solution.hpp"
+#include "../code/offer_37/solution.hpp"
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
-    Solution *test = new Solution();
+    Codec *test = new Codec();
+    
+    string t = "[1,2,3,4,null,null,5,5,null,1]";
 
-    cout << test->isPowerOfTwo(64) << endl;
+    TreeNode* head =  test->deserialize(t);
+    
     return 0;
 }

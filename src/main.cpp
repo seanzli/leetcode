08f854682bd72a39a8ed42720d58c4024a523587
seanzli <76615961+seanzli@users.noreply.github.com>
2021-05-30 17:55:35 +0800

    code 231

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 
-#include "../code/offer_13/solution.hpp"
+#include "../code221-240/code231/solution.hpp"
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
-    cout << test->movingCount(3,2,17) << endl;
+    cout << test->isPowerOfTwo(64) << endl;
     return 0;
 }

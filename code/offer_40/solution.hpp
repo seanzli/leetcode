1137f4800fbf5d413e151d5e0c9e6d92d893c51e
seanzli <76615961+seanzli@users.noreply.github.com>
2021-06-06 11:40:55 +0800

    code offer 40

--- code/offer_40/solution.hpp
+++ code/offer_40/solution.hpp
@@ -3,10 +3,11 @@
 #include <vector>
 #include <random>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
-class Solution {
+/* class Solution {
 public:
     vector<int> getLeastNumbers(vector<int>& arr, int k) {
         vector<int> out;
@@ -14,5 +15,23 @@ public:
         for (int i = 0; i<k;i++)
             out.push_back(arr[i]);
         return out;
+    }
+}; */
+
+class Solution {
+public:
+    vector<int> getLeastNumbers(vector<int>& arr, int k) {
+        priority_queue<int, vector<int>, greater<int>> que;
+        for (int i : arr) {
+            que.push(i);
+            if (que.size() > k)
+                que.pop();
+        }
+        vector<int> res;
+        while (que.size()) {
+            res.push_back(que.top());
+            que.pop();
+        }
+        return res;
     }
 };

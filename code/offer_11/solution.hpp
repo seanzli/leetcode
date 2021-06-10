30cc19c0137604a4f2899ac68955dd65355bf575
seanzli <76615961+seanzli@users.noreply.github.com>
2021-06-10 21:02:24 +0800

    code offer 11

---
+++ code/offer_11/solution.hpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minArray(vector<int>& numbers) {
+        int left = 0, right = numbers.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (numbers[mid] < numbers[right])
+                right = mid;
+            else if (numbers[mid] > numbers[right])
+                left = mid + 1;
+            else
+                right--;
+        }
+        return numbers[left];
+    }
+};

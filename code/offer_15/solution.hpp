29df7c7202151d078d40ef0e80918101b41648e4
seanzli <76615961+seanzli@users.noreply.github.com>
2021-06-12 16:59:42 +0800

    code offer 15

---
+++ code/offer_15/solution.hpp
@@ -0,0 +1,15 @@
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while (n > 0) {
+            n = (n & (n - 1));
+            count++;
+        }
+        return count;
+    }
+};

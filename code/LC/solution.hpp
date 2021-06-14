eed8fd103af0187497047ec6f7e61896317cc152
seanzli <76615961+seanzli@users.noreply.github.com>
2021-06-14 12:42:23 +0800

    lc set zeros

--- code/LC/solution.hpp
+++ code/LC/solution.hpp
@@ -34,3 +34,31 @@ public:
         return res;
     }
 };
+
+class Solution2 {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        if (matrix.size() == 0) return;
+        vector<int> raw;
+        vector<int> col;
+        for (int i = 0; i < matrix.size(); ++i) {
+            for (int j = 0; j < matrix[0].size(); ++j) {
+                if (matrix[i][j] == 0) {
+                    raw.push_back(i);
+                    col.push_back(j);
+                } 
+            }
+        }
+        for (int raw_index : raw) {
+            for (int j = 0; j < matrix[0].size(); ++j) {
+                matrix[raw_index][j] = 0;
+            }
+        }
+        for (int col_index : col) {
+            for (int i = 0; i < matrix.size(); ++i) {
+                matrix[i][col_index] = 0;
+            }
+        }
+        return;
+    }
+};

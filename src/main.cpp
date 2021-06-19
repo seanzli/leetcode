80b4977d3d3fcac3cc5aa8fda9283dffa25db50e
seanzli <76615961+seanzli@users.noreply.github.com>
2021-06-19 11:40:45 +0800

    offer 60

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 
-#include "../code523/solution.hpp"
+#include "../code1239/solution.hpp"
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
     
-    vector<int> head = {23,2,6,4,7};
-    int k = 13;
-    cout << test->checkSubarraySum(head, k) << endl;
+    vector<string> arr = {"cha","r","act","ers"};
+
+    cout << test->maxLength(arr) << endl;
     
     return 0;
 }

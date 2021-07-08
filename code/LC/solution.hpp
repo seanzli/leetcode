fedac889942f35f6b266d892722073419cc16e0e
seanzli <76615961+seanzli@users.noreply.github.com>
2021-07-08 21:07:52 +0800

    LC myPow

--- code/LC/solution.hpp
+++ code/LC/solution.hpp
@@ -328,12 +328,30 @@ using namespace std;
 //     }
 // };
 
+// class Solution {
+// public:
+//     int titleToNumber(string columnTitle) {
+//         long res = 0;
+//         for (int cur : columnTitle) {
+//             res = res *26 + cur - 'A' + 1;
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
-    int titleToNumber(string columnTitle) {
-        long res = 0;
-        for (int cur : columnTitle) {
-            res = res *26 + cur - 'A' + 1;
+    double myPow(double x, int n) {
+        if (n == 0)
+            return 1.0;
+        if (n < 0)
+            return 1.0 / myPow(x, -n);
+        double res = 1.0;
+        while (n > 0) {
+            if (n % 2 == 1)
+                res *= x;
+            x *= x;
+            n /= 2;
         }
         return res;
     }

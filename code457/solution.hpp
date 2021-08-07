2940d6a8442f62a016716c0c6192e7b31096c305
seanzli <76615961+seanzli@users.noreply.github.com>
2021-08-07 10:58:12 +0800

    code 457

--- code457/solution.hpp
+++ code457/solution.hpp
@@ -5,39 +5,68 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     const int INF = 1e8;
+//     bool dfs(vector<int>& nums, int i, int N, int dir) {
+//         // 遇到不符合条件的点，返回false
+//         if (nums[i] == 0) return false;
+//         // 遇到在考察中的点，说明形成了闭环，返回true
+//         if (nums[i] == INF) return true;
+//         int ndir = nums[i] > 0 ? 1 : -1;
+//         if (ndir == dir) {
+//             int j = (N + (nums[i] + i) % N) % N;
+//             // 将i处的值设为INF，标志i点在考察中
+//             nums[i] = INF;
+//             if (j != i && dfs(nums, j, N, dir)) {
+//                 return true;
+//             } else {
+//                 // i点不符合条件，赋值为0
+//                 nums[i] = 0;
+//                 return false;
+//             }
+//         }
+//         return false;
+//     }
+
+//     bool circularArrayLoop(vector<int>& nums) {
+//         int N = nums.size();
+//         for (int i = 0; i < N; ++i) {
+//             if (nums[i] == 0) continue;
+//             int dir = nums[i] > 0 ? 1 : -1;
+//             if (dfs(nums, i, N, dir)) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
-    const int INF = 1e8;
-    bool dfs(vector<int>& nums, int i, int N, int dir) {
-        // 遇到不符合条件的点，返回false
-        if (nums[i] == 0) return false;
-        // 遇到在考察中的点，说明形成了闭环，返回true
-        if (nums[i] == INF) return true;
-        int ndir = nums[i] > 0 ? 1 : -1;
-        if (ndir == dir) {
-            int j = (N + (nums[i] + i) % N) % N;
-            // 将i处的值设为INF，标志i点在考察中
-            nums[i] = INF;
-            if (j != i && dfs(nums, j, N, dir)) {
-                return true;
-            } else {
-                // i点不符合条件，赋值为0
-                nums[i] = 0;
+    bool circularArrayLoop(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0)
                 return false;
-            }
+            unordered_set<int> visited;
+            if (dfs(nums, visited, i, n, nums[i] > 0))
+                return true;
         }
         return false;
     }
-
-    bool circularArrayLoop(vector<int>& nums) {
-        int N = nums.size();
-        for (int i = 0; i < N; ++i) {
-            if (nums[i] == 0) continue;
-            int dir = nums[i] > 0 ? 1 : -1;
-            if (dfs(nums, i, N, dir)) {
-                return true;
-            }
-        }
+    
+    bool dfs(vector<int>& nums, unordered_set<int>& visited, const int i, const int n, const bool b_positive) {
+        if (nums[i] == 0)
+            return false;
+        if (visited.count(i) == 1)
+            return true;
+        visited.insert(i);
+        int idx = (n + (nums[i] + i) % n) % n;
+        if (nums[idx] == 0)
+            return false;
+        if (b_positive ^ (nums[idx] > 0)) // b_positive and nums[idx] have the same sign
+            return dfs(nums, visited, idx, n, b_positive);
         return false;
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "../code743/solution.hpp"
+#include "../code457/solution.hpp"
 
 #include <iostream>
 
@@ -6,9 +6,10 @@ int main(int argc, char* argv[])
 {
     Solution *test = new Solution();
 
-    vector<vector<int>> times = {{3,5,78},{2,1,1},{1,3,0},{4,3,59},{5,3,85},{5,2,22},{2,4,23},{1,4,43},{4,5,75},{5,1,15},{1,5,91},{4,1,16},{3,2,98},{3,4,22},{5,4,31},{1,2,0},{2,5,4},{4,2,51},{3,1,36},{2,3,59}};
+    
+    vector<int> nums = {-1, 2};
 
-    test->networkDelayTime(times, 5, 5);
+    test->circularArrayLoop(nums);
 
     return 0;
 }

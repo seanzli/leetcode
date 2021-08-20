bc3eb5c3d51bc16ee2a92cb17616a336f803ec02
seanzli <76615961+seanzli@users.noreply.github.com>
2021-08-21 07:55:23 +0800

    code 443

--- code345/solution.hpp
+++ code345/solution.hpp
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Sean
  * @Date: 2021-08-19 20:45:19
- * @LastEditTime: 2021-08-19 20:54:54
+ * @LastEditTime: 2021-08-21 07:49:49
  * @LastEditors: Sean
  * @Reference: 
  */
@@ -10,10 +10,12 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 class Solution {
-    bool isVowel(count char in) {
-        if ((in == 'a') || (in = 'A') || (in == 'e') || (in == 'E') ||
-           (in == 'i') || (in = 'I') || (in == 'o') || (in == 'O') ||
+    bool isVowel(const char in) {
+        if ((in == 'a') || (in == 'A') || (in == 'e') || (in == 'E') ||
+           (in == 'i') || (in == 'I') || (in == 'o') || (in == 'O') ||
            (in == 'u') || (in == 'U'))
            return true;
         return false;
--- code443/solution.hpp
+++ code443/solution.hpp
@@ -5,34 +5,59 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     int compress(vector<char>& chars) {
+//         char cur = chars[0];
+//         int size = 0;
+//         int count = 1;
+//         for (int i = 1; i < chars.size(); i++) {
+//             if (chars[i] == cur)
+//                 count++;
+//             else {
+//                 chars[size++] = cur;
+//                 if (count > 1) {
+//                     string str = to_string(count);
+//                     for (int j = 0; j < str.size(); j++) {
+//                         chars[size++] = str[j];
+//                     }
+//                 }
+//                 cur = chars[i];
+//                 count = 1;
+//             }
+//         }
+//         chars[size++] = cur;
+//         if (count > 1) {
+//             string str = to_string(count);
+//             for (int j = 0; j < str.size(); j++) {
+//                 chars[size++] = str[j];
+//             }
+//         }
+//         return size;
+//     }
+// };
+
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        char cur = chars[0];
-        int size = 0;
+        string res;
         int count = 1;
-        for (int i = 1; i < chars.size(); i++) {
-            if (chars[i] == cur)
+        char cur = chars[0];
+        for (int i = 1; i < chars.size(); ++i) {
+            if (chars[i] == cur) {
                 count++;
-            else {
-                chars[size++] = cur;
-                if (count > 1) {
-                    string str = to_string(count);
-                    for (int j = 0; j < str.size(); j++) {
-                        chars[size++] = str[j];
-                    }
-                }
+            } else {
+                res += cur;
+                if (count > 1)
+                    res += to_string(count);
                 cur = chars[i];
                 count = 1;
             }
         }
-        chars[size++] = cur;
-        if (count > 1) {
-            string str = to_string(count);
-            for (int j = 0; j < str.size(); j++) {
-                chars[size++] = str[j];
-            }
-        }
-        return size;
+        res += cur;
+        if (count > 1)
+            res += to_string(count);
+        chars = vector<char>(res.begin(), res.end());
+        return res.size();
     }
 };

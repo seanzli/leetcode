794a0ace4f4322d939c1bc079a99045e0a92671a
seanzli <76615961+seanzli@users.noreply.github.com>
2021-08-23 20:18:15 +0800

    code 1646

--- code1646/solution.hpp
+++ code1646/solution.hpp
@@ -6,11 +6,29 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     int getMaximumGenerated(int n) {
+//         vector<int> dp(n+1, 0);
+//         dp[1] = 1;
+//         int i = 1;
+//         while (2*i < n+1 ){
+//             dp[2*i] = dp[i];
+//             if (2*i+1 < n+1)
+//                 dp[2*i+1] = dp[i] + dp[i+1];
+//             i++;
+//         }
+//         return *max_element(dp.begin(), dp.end());
+//     }
+// };
+
 class Solution {
 public:
     int getMaximumGenerated(int n) {
-        vector<int> dp(n+1, 0);
-        dp[1] = 1;
+        if (n <= 1)
+            return n;
+        vector<int> dp(n + 1, 0);
+        dp[0] = 0; dp[1] = 1;
         int i = 1;
         while (2*i < n+1 ){
             dp[2*i] = dp[i];

f0dc2200a254b13c58343852891b65eb3ddb6cf6
seanzli <76615961+seanzli@users.noreply.github.com>
2021-09-21 10:34:38 +0800

    code 58

--- code1-100/code41-60/code58/solution.hpp
+++ code1-100/code41-60/code58/solution.hpp
@@ -9,26 +9,41 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     int lengthOfLastWord(string s) {
+//         string cur;
+        
+//         int i = s.size()-1;
+//         for (; i>=0; --i)
+//         {
+//             if (s[i] == ' ')
+//                 continue;
+//             else
+//                 break;
+//         }
+
+//         for (int j = i; j>=0; --j)
+//         {
+//             if (s[j] == ' ')
+//                 return cur.size();
+//             cur = cur + s[j];
+//         }
+//         return cur.size();
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        string cur;
-        
-        int i = s.size()-1;
-        for (; i>=0; --i)
-        {
-            if (s[i] == ' ')
-                continue;
-            else
-                break;
+        int res = 0;
+        int idx = s.size() - 1;
+        while (idx >= 0 && s[idx] == ' ')
+            --idx;
+        while (idx >= 0 && s[idx] != ' ') {
+            --idx;
+            ++res;
         }
-
-        for (int j = i; j>=0; --j)
-        {
-            if (s[j] == ' ')
-                return cur.size();
-            cur = cur + s[j];
-        }
-        return cur.size();
+        return res;
     }
 };

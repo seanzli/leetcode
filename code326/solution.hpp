0a25f4552e22580f95eb1400562369d1c7ef4fbc
seanzli <76615961+seanzli@users.noreply.github.com>
2021-09-23 20:09:18 +0800

    code326

---
+++ code326/solution.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0)return false;
+        while (n > 1) {
+            if (n % 3 == 0)
+                n /= 3;
+            else
+                return false;
+        }
+        return n == 1;
+    }
+};

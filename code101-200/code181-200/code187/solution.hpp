55e32722d73bcce76501fa624c8b790178586475
seanzli <76615961+seanzli@users.noreply.github.com>
2021-10-08 20:52:14 +0800

    code 187

--- code101-200/code181-200/code187/solution.hpp
+++ code101-200/code181-200/code187/solution.hpp
@@ -8,15 +8,36 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     vector<string> findRepeatedDnaSequences(string s) {
+//         unordered_map<string,int>mp;
+//         vector<string>res;
+//         for(int i=0;i+9<s.size();i++){
+//             auto key=s.substr(i,10);
+//             if(mp[key]==1)res.push_back(key);
+//                 mp[key]++;
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-        unordered_map<string,int>mp;
-        vector<string>res;
-        for(int i=0;i+9<s.size();i++){
-            auto key=s.substr(i,10);
-            if(mp[key]==1)res.push_back(key);
-                mp[key]++;
+        unordered_map<string, int> hash;
+        if (s.size() < 10)
+            return {};
+        string cur = s.substr(0, 9);
+        for (int i = 9; i < s.size(); ++i) {
+            cur.push_back(s[i]);
+            hash[cur]++;
+            cur = cur.substr(1);
+        }
+        vector<string> res;
+        for (auto itr = hash.begin(); itr != hash.end(); ++itr) {
+            if (itr->second > 1)
+                res.push_back(itr->first);
         }
         return res;
     }

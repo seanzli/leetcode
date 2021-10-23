e8660581db65f2d0f98649cd2a64f23c26ffa3ae
mu <muxzuo@outlook.com>
2021-10-23 09:09:36 +0800

    code 492

---
+++ code492/solution.hpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int w = sqrt(1.0 * area);
+        while (area % w) {
+            --w;
+        }
+        return {area / w, w};
+    }
+};

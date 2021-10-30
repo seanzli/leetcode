715d705b2e6a3ee7b01db2705580ad53dcbe0405
mu <muxzuo@outlook.com>
2021-10-30 16:55:14 +0800

    code 260

--- code241-260/code260/solution.hpp
+++ code241-260/code260/solution.hpp
@@ -3,17 +3,34 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
+
+// class Solution {
+// public:
+//     vector<int> singleNumber(vector<int>& nums) {
+//         unordered_set<int> hash;
+//         for (int i : nums){
+//             if (!hash.insert(i).second)
+//                 hash.erase(i);
+//         }
+//         return vector<int>(hash.begin(), hash.end());
+//     }
+// };
 
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        unordered_set<int> hash;
-        for (int i : nums){
-            if (!hash.insert(i).second)
-                hash.erase(i);
+        unordered_map<int, int> hash;
+        for (int& itr : nums) {
+            hash[itr]++;
         }
-        return vector<int>(hash.begin(), hash.end());
+        vector<int> res{};
+        for (auto itr = hash.begin(); itr != hash.end(); ++itr) {
+            if (itr->second == 1)
+                res.push_back(itr->first);
+        }
+        return res;
     }
 };

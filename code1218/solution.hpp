4fe59c52637ed197ceb300867c7f0123efef3694
mu <muxzuo@outlook.com>
2021-11-05 21:10:33 +0800

    code 1218

---
+++ code1218/solution.hpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestSubsequence(vector<int> &arr, int difference) {
+        int ans = 0;
+        unordered_map<int, int> dp;
+        for (int v: arr) {
+            dp[v] = dp[v - difference] + 1;
+            ans = max(ans, dp[v]);
+        }
+        return ans;
+    }
+};

026c97560382e577d47a968b2d2b2d0f1eac8330
Mu <76615961+seanzli@users.noreply.github.com>
2021-11-06 11:30:36 +0800

    code 268

--- code261-300/code268/solution.hpp
+++ code261-300/code268/solution.hpp
@@ -3,16 +3,28 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         unordered_set<int> hash;
+//         for (int i = 0; i <= nums.size(); ++i) {
+//             hash.insert(i);
+//         }
+//         for (auto& i : nums)
+//             hash.erase(i);
+
+//         return *hash.begin();
+//     }
+// };
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        unordered_set<int> hash;
-        for (int i = 0; i <= nums.size(); ++i) {
-            hash.insert(i);
+        int res = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            res ^= i;
+            res ^= nums[i];
         }
-        for (auto& i : nums)
-            hash.erase(i);
-
-        return *hash.begin();
+        return res ^ nums.size();
     }
 };

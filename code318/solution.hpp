b45448d20e95a50a6d06c9c237bfc2ef38e25f19
mu <muxzuo@outlook.com>
2021-11-17 20:24:28 +0800

    code 318

--- code318/solution.hpp
+++ code318/solution.hpp
@@ -32,24 +32,48 @@ public:
     }
 }; */
 
+// class Solution {
+// public:
+//     int maxProduct(vector<string>& words) {
+//         if (words.size() < 2)   return 0;
+//         vector<int> mark;
+//         for (string str : words) {
+//             int cur_mark = 0;
+//             for (char cur : str)
+//                 cur_mark |= (1 << (cur - 'a'));
+//             mark.push_back(cur_mark);
+//         }
+//         int max_prod = 0;
+//         for (int i = 0; i < words.size() - 1; i++) {
+//             for (int j = i + 1; j < words.size(); j++) {
+//                 if (!(mark[i] & mark[j]))
+//                     max_prod = max(max_prod, (int)(words[i].size() * words[j].size()));
+//             }
+//         }
+//         return max_prod;
+//     }
+// };
+
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
-        if (words.size() < 2)   return 0;
-        vector<int> mark;
-        for (string str : words) {
-            int cur_mark = 0;
-            for (char cur : str)
-                cur_mark |= (1 << (cur - 'a'));
-            mark.push_back(cur_mark);
-        }
-        int max_prod = 0;
-        for (int i = 0; i < words.size() - 1; i++) {
-            for (int j = i + 1; j < words.size(); j++) {
-                if (!(mark[i] & mark[j]))
-                    max_prod = max(max_prod, (int)(words[i].size() * words[j].size()));
+        int length = words.size();
+        vector<int> masks(length);
+        for (int i = 0; i < length; i++) {
+            string word = words[i];
+            int wordLength = word.size();
+            for (int j = 0; j < wordLength; j++) {
+                masks[i] |= 1 << (word[j] - 'a');
             }
         }
-        return max_prod;
+        int maxProd = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if ((masks[i] & masks[j]) == 0) {
+                    maxProd = max(maxProd, int(words[i].size() * words[j].size()));
+                }
+            }
+        }
+        return maxProd;
     }
 };

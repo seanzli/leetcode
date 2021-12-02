fdbce12a0e187aad9690af7b5d2d9832ff362d1a
mu <muxzuo@outlook.com>
2021-12-02 20:30:24 +0800

    code 506

---
+++ code506/solution.hpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        using PII = std::pair<int, int>;
+        std::vector<PII> hash(score.size());
+        for (int i = 0; i < score.size(); ++i) {
+            hash[i] = std::make_pair(score[i], i);
+        }
+        std::sort(hash.begin(), hash.end(), 
+            [](const PII& a, const PII& b) {
+                return a.first > b.first;
+            }
+        );
+
+        vector<string> res(hash.size());
+        for (int i = 0; i < hash.size(); ++i) {
+            res[hash[i].second] = to_string(i + 1);
+        }
+        res[hash[0].second] = "Gold Medal";
+        if (res.size() > 1)
+            res[hash[1].second] = "Silver Medal";
+        if (res.size() > 2)
+            res[hash[2].second] = "Bronze Medal";
+        return res;
+    }
+};

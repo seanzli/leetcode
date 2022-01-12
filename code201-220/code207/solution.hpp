f5b3e667e4440662a19f796bbb5955d716512299
mu <muxzuo@outlook.com>
2022-01-12 21:38:20 +0800

    code 207

--- code201-220/code207/solution.hpp
+++ code201-220/code207/solution.hpp
@@ -3,46 +3,161 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <queue>
+
+#include <stack>
 
 using namespace std;
 
 
+//class Solution {
+//private:
+//    vector<vector<int>> edges;
+//    vector<int> visited;
+//    bool valid = true;
+//
+//public:
+//    void dfs(int u) {
+//        visited[u] = 1;
+//        for (int v: edges[u]) {
+//            if (visited[v] == 0) {
+//                dfs(v);
+//                if (!valid) {
+//                    return;
+//                }
+//            }
+//            else if (visited[v] == 1) {
+//                valid = false;
+//                return;
+//            }
+//        }
+//        visited[u] = 2;
+//    }
+//
+//    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//        edges.resize(numCourses);
+//        visited.resize(numCourses);
+//        for (const auto& info: prerequisites) {
+//            edges[info[1]].push_back(info[0]);
+//        }
+//        for (int i = 0; i < numCourses && valid; ++i) {
+//            if (!visited[i]) {
+//                dfs(i);
+//            }
+//        }
+//        return valid;
+//    }
+//};
+
+//class Solution {
+//public:
+//    unordered_map<int, vector<int>> hash; // course -> base course;
+//    unordered_set<int> visited;
+//    unordered_set<int> node;
+//    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//        for (const auto& itr : prerequisites) {
+//            hash[itr[1]].emplace_back(itr[0]);
+//            node.insert(itr[0]);
+//            node.insert(itr[1]);
+//        }
+//        stack<int> stack_;
+//        for (const auto& itr : node) {
+//            if (!visited.count(itr))
+//                topologicalSort(itr, stack_);
+//        }
+//
+//        return stack_.size() == numCourses;
+//    }
+//
+//private:
+//    void topologicalSort(int course, stack<int>& s) {
+//        visited.insert(course);
+//        for (const auto& itr : hash[course]) {
+//            if (!visited.count(itr)) {
+//                topologicalSort(itr, s);
+//            }
+//        }
+//        s.push(course);
+//    }
+//};
+
+//class Solution {
+//    unordered_map<int, unordered_set<int>> hash; // course -> base;
+//public:
+//    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//        for (const auto& itr : prerequisites) {
+//            hash[itr[1]].insert(itr[0]);
+//            hash[itr[0]];
+//        }
+//
+//        while (!hash.empty()) {
+//            int root = getRoot();
+//            if (root == -1)
+//                return false;
+//            hash.erase(root);
+//            deleteRoot(root);
+//        }
+//        return true;
+//    }
+//
+//private:
+//    int getRoot() {
+//        for (auto& itr : hash) {
+//            if (itr.second.empty())
+//                return itr.first;
+//        }
+//        return -1;
+//    }
+//
+//    void deleteRoot(int root) {
+//        for (auto& itr : hash) {
+//            if (itr.second.count(root))
+//                itr.second.erase(root);
+//        }
+//    }
+//};
+
 class Solution {
-private:
-    vector<vector<int>> edges;
-    vector<int> visited;
+    unordered_map<int, vector<int>> hash; // course -> first course vec
     bool valid = true;
-
+    unordered_set<int> safe;
 public:
-    void dfs(int u) {
-        visited[u] = 1;
-        for (int v: edges[u]) {
-            if (visited[v] == 0) {
-                dfs(v);
-                if (!valid) {
-                    return;
-                }
-            }
-            else if (visited[v] == 1) {
-                valid = false;
-                return;
-            }
-        }
-        visited[u] = 2;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        edges.resize(numCourses);
-        visited.resize(numCourses);
-        for (const auto& info: prerequisites) {
-            edges[info[1]].push_back(info[0]);
+        for (const auto& itr : prerequisites) {
+            hash[itr[0]].emplace_back(itr[1]);
         }
-        for (int i = 0; i < numCourses && valid; ++i) {
-            if (!visited[i]) {
-                dfs(i);
-            }
+        if (hash.empty())
+            return true;
+        unordered_set<int> visited;
+        for (const auto& itr : hash) {
+            if (safe.count(itr.first))
+                continue;
+            dfs(itr.first, visited);
+            if (valid)
+                safe.insert(itr.first);
+
         }
         return valid;
+    }
+
+private:
+    void dfs(const int course, unordered_set<int>& visited) {
+        if (!valid)
+            return;
+        if (visited.count(course)) {
+            valid = false;
+            return;
+        }
+
+        visited.insert(course);
+        for (size_t i = 0; i < hash[course].size() && valid; ++i) {
+            if (safe.count(hash[course][i]))
+                continue;
+            dfs(hash[course][i], visited);
+            if (valid)
+                safe.insert(hash[course][i]);
+        }
+        visited.erase(course);
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
-#include "../code1-100/code61-80/code71/solution.hpp"
+#include "../code201-220/code207/solution.hpp"
 
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
     Solution s;
-    string path = "/..";
-    std::cout << s.simplifyPath(path) << std::endl;
+    vector<vector<int>> prerequisites = 
+    {{0,10},{3,18},{5,5},{6,11},{11,14},{13,1},{15,1},{17,4}};
+    std::cout << s.canFinish(20, prerequisites) << std::endl;
     return 0;
 }

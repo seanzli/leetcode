b500cae5ee83edcfe9386f9db8cb5f9a2a82a3fb
mu <muxzuo@outlook.com>
2022-01-19 22:19:25 +0800

    code 219

--- code201-220/code219/solution.hpp
+++ code201-220/code219/solution.hpp
@@ -2,24 +2,45 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 #include <numeric>
 #include <limits.h>
 
 using namespace std;
 
+//class Solution {
+//public:
+//    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//        unordered_map<int,int> hash;
+//        int min_target = INT_MAX;
+//        for (int i = 0; i < nums.size(); i++)
+//        {
+//            if (hash.find(nums[i])!= hash.end()){
+//                min_target = min(i-hash[nums[i]], min_target);
+//                if (min_target <= k)
+//                    return true;
+//            }
+//            hash[nums[i]] = i;
+//        }
+//        return false;
+//    }
+//};
+
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,int> hash;
-        int min_target = INT_MAX;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (hash.find(nums[i])!= hash.end()){
-                min_target = min(i-hash[nums[i]], min_target);
-                if (min_target <= k)
-                    return true;
-            }
-            hash[nums[i]] = i;
+        unordered_set<int> hash;
+        int right = 0;
+        for (right = 0; right < k; ++right) {
+            if (hash.count(nums[right]))
+                return true;
+            hash.insert(nums[right]);
+        }
+        for (int left = 0; right < nums.size(); ++right, ++left) {
+            if (hash.count(nums[right]))
+                return true;
+            hash.insert(nums[right]);
+            hash.erase(nums[left]);
         }
         return false;
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
-#include "../code201-220/code207/solution.hpp"
+#include "../code201-220/code219/solution.hpp"
 
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
     Solution s;
-    vector<vector<int>> prerequisites = 
-    {{0,10},{3,18},{5,5},{6,11},{11,14},{13,1},{15,1},{17,4}};
-    std::cout << s.canFinish(20, prerequisites) << std::endl;
+    vector<int> num = {1,2,3,4,5,6,7,8,9,10};
+    std::cout << s.containsNearbyDuplicate(num, 15) << std::endl;
     return 0;
 }

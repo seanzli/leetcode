5e4b650ea3ec01878a0c6f1ac70790a5fcf98f17
mu <muxzuo@outlook.com>
2022-02-04 11:18:50 +0800

    code 1725

--- code1725/solution.hpp
+++ code1725/solution.hpp
@@ -1,16 +1,29 @@
 #include <algorithm>
 #include <vector>
 #include <map>
+#include <numeric>
 
 using namespace std;
+
+//class Solution {
+//public:
+//    int countGoodRectangles(vector<vector<int>>& rectangles) {
+//        map<int, int> hash; // square , count;
+//        for (const auto& itr : rectangles)
+//            ++hash[std::min(itr[0], itr[1])];
+//
+//        return hash.rbegin()->second;
+//    }
+//};
 
 class Solution {
 public:
     int countGoodRectangles(vector<vector<int>>& rectangles) {
-        map<int, int> hash; // square , count;
-        for (const auto& itr : rectangles)
-            ++hash[std::min(itr[0], itr[1])];
-
-        return hash.rbegin()->second;
+        auto& itr = *max_element(rectangles.begin(), rectangles.end(), [](const vector<int>& a, const vector<int>& b){
+            return min(a[0], a[1]) > min(b[0], b[1]);
+        });
+        return count_if(rectangles.begin(), rectangles.end(), [&](const vector<int>& vec) {
+            return min(vec[0], vec[1]) == min(itr[0], itr[1]);
+        });
     }
 };

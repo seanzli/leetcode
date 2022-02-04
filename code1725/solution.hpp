b04b120f06af4656803f0b3ffd2afb3a35243576
mu <muxzuo@outlook.com>
2022-02-04 11:19:04 +0800

    code 1725

--- code1725/solution.hpp
+++ code1725/solution.hpp
@@ -20,7 +20,7 @@ class Solution {
 public:
     int countGoodRectangles(vector<vector<int>>& rectangles) {
         auto& itr = *max_element(rectangles.begin(), rectangles.end(), [](const vector<int>& a, const vector<int>& b){
-            return min(a[0], a[1]) > min(b[0], b[1]);
+            return min(a[0], a[1]) < min(b[0], b[1]);
         });
         return count_if(rectangles.begin(), rectangles.end(), [&](const vector<int>& vec) {
             return min(vec[0], vec[1]) == min(itr[0], itr[1]);

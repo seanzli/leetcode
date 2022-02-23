72325388689e7b9b9a5888b9fa08ed1d38c573bd
mu <muxzuo@outlook.com>
2022-02-23 20:14:42 +0800

    code 917

--- code917/solution.hpp
+++ code917/solution.hpp
@@ -4,44 +4,63 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    string reverseOnlyLetters(string S) {
+//        char *out = (char*)S.c_str();
+//        char temp = '0';
+//
+//        for (int idx1 = 0, idx2=S.size()-1;idx1<idx2;)
+//        {
+//            if (temp=='0')
+//            {   if(((out[idx1]>='a') &&(out[idx1]<='z'))
+//                        || ((out[idx1]>='A') &&(out[idx1]<='Z')))
+//                {
+//                    temp = out[idx1];
+//                }
+//                else
+//                {
+//                    idx1++;
+//                    continue;
+//                }
+//            }
+//
+//
+//            if(((out[idx2]>='a') &&(out[idx2]<='z'))
+//                || ((out[idx2]>='A') &&(out[idx2]<='Z')))
+//            {
+//                out[idx1] = out[idx2];
+//                out[idx2] = temp;
+//                temp = '0';
+//                idx1++;
+//                idx2--;
+//                continue;
+//            }
+//            else
+//            {
+//                idx2--;
+//                continue;
+//            }
+//        }
+//        return S;
+//    }
+//};
+
 class Solution {
 public:
-    string reverseOnlyLetters(string S) {
-        char *out = (char*)S.c_str();
-        char temp = '0';
-
-        for (int idx1 = 0, idx2=S.size()-1;idx1<idx2;)
-        {
-            if (temp=='0')
-            {   if(((out[idx1]>='a') &&(out[idx1]<='z')) 
-                        || ((out[idx1]>='A') &&(out[idx1]<='Z')))
-                {
-                    temp = out[idx1];
-                }
-                else
-                {
-                    idx1++;
-                    continue;
-                }
+    string reverseOnlyLetters(string s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            if (!isalpha(s[left])) {
+                ++left;
+            } else if (!isalpha(s[right])) {
+                --right;
+            } else {
+                swap(s[left], s[right]);
+                ++left;
+                --right;
             }
-            
-
-            if(((out[idx2]>='a') &&(out[idx2]<='z')) 
-                || ((out[idx2]>='A') &&(out[idx2]<='Z')))
-            {
-                out[idx1] = out[idx2];
-                out[idx2] = temp;
-                temp = '0';
-                idx1++;
-                idx2--;
-                continue;
-            }
-            else
-            {
-                idx2--;
-                continue;
-            } 
         }
-        return S;
+        return s;
     }
 };

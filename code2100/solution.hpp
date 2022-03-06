3de4da5ed01c056b5f79f2eaf6a810e6742857f9
mu <muxzuo@outlook.com>
2022-03-06 10:46:46 +0800

    code 2100

--- code2100/solution.hpp
+++ code2100/solution.hpp
@@ -6,21 +6,21 @@ class Solution {
 public:
     vector<int> goodDaysToRobBank(vector<int>& security, int time) {
         int n = security.size();
-        vector<int> g(n, 0);
+        vector<int> g(n, 0); // 1 : increase, 0 = no change, -1 = decrease
         for (int i = 1; i < n; ++i) {
             if (security[i] == security[i - 1])
                 continue;
             g[i] = security[i] > security[i - 1] ? 1 : -1;
         }
-        vector<int> left(n + 1, 0), right(n + 1, 0);
+        vector<int> increase(n + 1, 0), decrease(n + 1, 0);
         for (int i = 1; i <= n; ++i) {
-            left[i] = left[i - 1] + (g[i - 1] == 1 ? 1 : 0);
-            right[i] = right[i - 1] + (g[i - 1] == -1 ? 1 : 0);
+            increase[i] = increase[i - 1] + (g[i - 1] == 1 ? 1 : 0); // increase count
+            decrease[i] = decrease[i - 1] + (g[i - 1] == -1 ? 1 : 0); // decrease count
         }
         vector<int> res;
         for (int i = time; i < n - time; ++i) {
-            int c1 = left[i + 1] - left[i + 1 - time];
-            int c2 = right[i + 1 + time] - right[i + 1];
+            int c1 = increase[i + 1] - increase[i + 1 - time]; // increase count in [i - time , i]
+            int c2 = decrease[i + 1 + time] - decrease[i + 1]; // decrease count in [i, i + time];
             if (c1 == 0 && c2 == 0)
                 res.push_back(i);
         }

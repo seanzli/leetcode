729f3fbcf0034792b1466307183701d2cd35a698
mu <muxzuo@outlook.com>
2022-03-17 21:50:52 +0800

    code 720

--- code720/solution.hpp
+++ code720/solution.hpp
@@ -5,46 +5,101 @@
 
 using namespace std;
 
-class Solution {
-    struct Tire {
-        bool isEnd;
-        vector<Tire*> child;
-        Tire() {
-            isEnd = false;
-            child = vector<Tire*>(26, nullptr);
-        };
-    };
-public:
-    string longestWord(vector<string>& words) {
-        Tire* root = new Tire();
-        for (string str : words) {
-            Tire* cur = root;
-            for (char i : str) {
-                if (cur->child[i - 'a'] == nullptr)
-                    cur->child[i - 'a'] = new Tire();
-                cur = cur->child[i - 'a'];
-            }
-            cur->isEnd = true;
-        }
+//class Solution {
+//    struct Tire {
+//        bool isEnd;
+//        vector<Tire*> child;
+//        Tire() {
+//            isEnd = false;
+//            child = vector<Tire*>(26, nullptr);
+//        };
+//    };
+//public:
+//    string longestWord(vector<string>& words) {
+//        Tire* root = new Tire();
+//        for (string str : words) {
+//            Tire* cur = root;
+//            for (char i : str) {
+//                if (cur->child[i - 'a'] == nullptr)
+//                    cur->child[i - 'a'] = new Tire();
+//                cur = cur->child[i - 'a'];
+//            }
+//            cur->isEnd = true;
+//        }
+//
+//        string res = "";
+//        string cur = "";
+//        dfs(root, res, cur);
+//        return res;
+//    }
+//
+//    void dfs(Tire* root, string& res, string& cur) {
+//        for (int i = 0; i < 26; ++i) {
+//            if (root->child[i] == nullptr)
+//                continue;
+//            if (root->child[i]->isEnd == true) {
+//                cur += (i + 'a');
+//                dfs(root->child[i], res, cur);
+//                if (res.size() < cur.size()) {
+//                    res = cur;
+//                }
+//                cur.pop_back();
+//            }
+//        }
+//    }
+//};
 
-        string res = "";
-        string cur = "";
-        dfs(root, res, cur);
-        return res;
+class Trie {
+public:
+    Trie() {
+        this->children = vector<Trie *>(26, nullptr);
+        this->isEnd = false;
     }
 
-    void dfs(Tire* root, string& res, string& cur) {
-        for (int i = 0; i < 26; ++i) {
-            if (root->child[i] == nullptr)
-                continue;
-            if (root->child[i]->isEnd == true) {
-                cur += (i + 'a');
-                dfs(root->child[i], res, cur);
-                if (res.size() < cur.size()) {
-                    res = cur;
+    bool insert(const string & word) {
+        Trie * node = this;
+        for (const auto & ch : word) {
+            int index = ch - 'a';
+            if (node->children[index] == nullptr) {
+                node->children[index] = new Trie();
+            }
+            node = node->children[index];
+        }
+        node->isEnd = true;
+        return true;
+    }
+
+    bool search(const string & word) {
+        Trie * node = this;
+        for (const auto & ch : word) {
+            int index = ch - 'a';
+            if (node->children[index] == nullptr || !node->children[index]->isEnd) {
+                return false;
+            }
+            node = node->children[index];
+        }
+        return node != nullptr && node->isEnd;
+    }
+private:
+    vector<Trie *> children;
+    bool isEnd;
+};
+
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        Trie trie;
+        for (const auto & word : words) {
+            trie.insert(word);
+        }
+        string longest = "";
+        for (const auto & word : words) {
+            if (trie.search(word)) {
+                if (word.size() > longest.size() || (word.size() == longest.size() && word < longest)) {
+                    longest = word;
                 }
-                cur.pop_back();
             }
         }
+        return longest;
     }
 };

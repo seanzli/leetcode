948b4d7ec8f4caed5dcbff7dbf508fa88119caf8
mu <muxzuo@outlook.com>
2022-03-26 10:59:04 +0800

    code 682

--- code682/solution.hpp
+++ code682/solution.hpp
@@ -8,15 +8,15 @@ class Solution {
 public:
     int calPoints(vector<string>& ops) {
         vector<int> s;
-        for (int i = 0; i < ops.size(); ++i) {
-            if (ops[i] == "+" && s.size() > 1)
+        for (auto & op : ops) {
+            if (op == "+" && s.size() > 1)
                 s.emplace_back(s[s.size() - 2] + s[s.size() - 1]);
-            else if (ops[i] == "C" && !s.empty())
+            else if (op == "C" && !s.empty())
                 s.pop_back();
-            else if (ops[i] == "D" && !s.empty())
+            else if (op == "D" && !s.empty())
                 s.emplace_back(s.back() * 2);
             else
-                s.emplace_back(stoi(ops[i]));
+                s.emplace_back(stoi(op));
         }
         return accumulate(s.begin(), s.end(), 0);
     }

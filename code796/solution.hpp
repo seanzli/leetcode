6117508d8435319d7d48ed758b9c5477bbbb33cc
mu <muxzuo@outlook.com>
2022-04-07 20:47:56 +0800

    code 796

---
+++ code796/solution.hpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        return s.size() == goal.size() && (s + s).find(goal) != string::npos;
+    }
+};

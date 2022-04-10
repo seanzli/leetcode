864b25a6643e7f46f7a0d397d249499551447b40
mu <muxzuo@outlook.com>
2022-04-10 10:09:21 +0800

    code 804

--- code804/solution.hpp
+++ code804/solution.hpp
@@ -6,19 +6,41 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    int uniqueMorseRepresentations(vector<string>& words) {
+//        vector<string> map = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+//        unordered_set<string> hash;
+//        string temp;
+//        for (string i : words){
+//            temp = "";
+//            for (char cur : i){
+//                temp += map[cur-'a'];
+//            }
+//            hash.insert(temp);
+//        }
+//        return temp.size();
+//    }
+//};
+
+static const string g_map[26] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        vector<string> map = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        unordered_set<string> hash;
-        string temp;
-        for (string i : words){
-            temp = "";
-            for (char cur : i){
-                temp += map[cur-'a'];
-            }
-            hash.insert(temp);
+        unordered_set<string> _set;
+        for (const auto& str : words) {
+            string cur = trans(str);
+            if (_set.count(cur) == 0)
+                _set.insert(cur);
         }
-        return temp.size();
+        return _set.size();
+    }
+
+    string trans(const string& word) {
+        string out = "";
+        for (const auto& ch : word) {
+            out += g_map[ch - 'a'];
+        }
+        return out;
     }
 };

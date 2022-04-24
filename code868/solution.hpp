e04da2c53eb0880ef372d839cc9bc8a96c466d3e
mu <muxzuo@outlook.com>
2022-04-24 20:44:53 +0800

    code 868

--- code868/solution.hpp
+++ code868/solution.hpp
@@ -5,25 +5,42 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    int binaryGap(int n) {
+//        bitset<32> bit_s(n);
+//        string str = bit_s.to_string();
+//        int max_dis = 0;
+//        if (bit_s.count() < 2)
+//            return 0;
+//        int idx = -1;
+//        for (int i=0; i<str.size(); i++){
+//            if (str[i] == '1'){
+//                if (idx == -1){
+//                    idx = i;
+//                } else {
+//                    max_dis = max(max_dis, i-idx);
+//                    idx = i;
+//                }
+//            }
+//        }
+//        return idx;
+//    }
+//};
+
 class Solution {
 public:
     int binaryGap(int n) {
-        bitset<32> bit_s(n);
-        string str = bit_s.to_string();
-        int max_dis = 0;
-        if (bit_s.count() < 2)
-            return 0;
-        int idx = -1;
-        for (int i=0; i<str.size(); i++){
-            if (str[i] == '1'){
-                if (idx == -1){
-                    idx = i;
-                } else {
-                    max_dis = max(max_dis, i-idx);
-                    idx = i;
+        int last = -1, ans = 0;
+        for (int i = 0; n; ++i) {
+            if (n & 1) {
+                if (last != -1) {
+                    ans = max(ans, i - last);
                 }
+                last = i;
             }
+            n >>= 1;
         }
-        return idx;
+        return ans;
     }
 };

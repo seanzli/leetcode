40729d2035c7a0fbb2919af6e17307dbdc179939
mu <muxzuo@outlook.com>
2022-05-17 20:51:59 +0800

    code 953

--- code953/solution.hpp
+++ code953/solution.hpp
@@ -6,23 +6,53 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    bool isAlienSorted(vector<string>& words, string order) {
+//        unordered_map<char, int> hash;
+//        for (int i = 0; i < order.size(); i++) {
+//            hash[order[i]] = i;
+//        }
+//        for (int i = 0; i < words.size() - 1; i++) {
+//            int idx = 0;
+//            for ( ; idx < words[i].size() && idx < words[i + 1].size(); idx++) {
+//                if (hash[words[i][idx]] > hash[words[i + 1][idx]])
+//                    return false;
+//                if (hash[words[i][idx]] < hash[words[i + 1][idx]])
+//                    break;
+//            }
+//            if (idx == min(words[i].size(), words[i + 1].size()) && words[i + 1].size() < words[i].size()) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//};
+
 class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order) {
-        unordered_map<char, int> hash;
+        vector<int> index(26);
         for (int i = 0; i < order.size(); i++) {
-            hash[order[i]] = i;
+            index[order[i] - 'a'] = i;
         }
-        for (int i = 0; i < words.size() - 1; i++) {
-            int idx = 0;
-            for ( ; idx < words[i].size() && idx < words[i + 1].size(); idx++) {
-                if (hash[words[i][idx]] > hash[words[i + 1][idx]])
-                    return false;
-                if (hash[words[i][idx]] < hash[words[i + 1][idx]])
+        for (int i = 1; i < words.size(); i++) {
+            bool valid = false;
+            for (int j = 0; j < words[i - 1].size() && j < words[i].size(); j++) {
+                int prev = index[words[i - 1][j] - 'a'];
+                int curr = index[words[i][j] - 'a'];
+                if (prev < curr) {
+                    valid = true;
                     break;
+                } else if (prev > curr) {
+                    return false;
+                }
             }
-            if (idx == min(words[i].size(), words[i + 1].size()) && words[i + 1].size() < words[i].size()) {
-                return false;
+            if (!valid) {
+                /* 比较两个字符串的长度 */
+                if (words[i - 1].size() > words[i].size()) {
+                    return false;
+                }
             }
         }
         return true;

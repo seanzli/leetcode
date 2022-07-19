2a27ba2169cc2a77478fe8fae88949c2b97b7a31
mu <muxzuo@outlook.com>
2022-07-19 20:21:32 +0800

    code 731

---
+++ code731/solution.hpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+using namespace std;
+
+class MyCalendarTwo {
+public:
+    MyCalendarTwo() {
+
+    }
+
+    bool book(int start, int end) {
+        for (auto &[l, r] : overlaps) {
+            if (l < end && start < r) {
+                return false;
+            }
+        }
+        for (auto &[l, r] : booked) {
+            if (l < end && start < r) {
+                overlaps.emplace_back(max(l, start), min(r, end));
+            }
+        }
+        booked.emplace_back(start, end);
+        return true;
+    }
+private:
+    vector<pair<int, int>> booked;
+    vector<pair<int, int>> overlaps;
+};

484167355ccdc85ac8829cb2515ab0dbd5f4aa6c
mu <muxzuo@outlook.com>
2022-08-01 21:19:43 +0800

    code 1374

---
+++ code1374/solution.hpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string generateTheString(int n) {
+        if (n % 2 == 1) {
+            return string(n, 'a');
+        }
+        return string(n - 1, 'a') + 'b';
+    }
+};

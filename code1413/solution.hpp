e0fe16cae97a64f75ebfcc4121e285857d13f41c
mu <muxzuo@outlook.com>
2022-08-09 19:53:01 +0800

    code 1413

--- code1413/solution.hpp
+++ code1413/solution.hpp
@@ -6,15 +6,28 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    int minStartValue(vector<int>& nums) {
+//        int min_value = max(1-nums[0], 1);
+//        for (int i=1 ;i<nums.size(); i++){
+//            nums[i] += nums[i-1];
+//            if (nums[i] <= 0)
+//                min_value = max(min_value, (int)(ceil((1-nums[i]))));
+//        }
+//        return min_value;
+//    }
+//};
+
 class Solution {
 public:
     int minStartValue(vector<int>& nums) {
-        int min_value = max(1-nums[0], 1);
-        for (int i=1 ;i<nums.size(); i++){
-            nums[i] += nums[i-1];
-            if (nums[i] <= 0)
-                min_value = max(min_value, (int)(ceil((1-nums[i]))));
+        int sum = 0, cur = 0;
+        for (const int& num : nums) {
+            sum += num;
+            if (sum < cur)
+                cur = sum;
         }
-        return min_value;
+        return 1 - cur;
     }
 };

bd82a40c2f97fe41cdb6e56f95e720fd720d2b8b
mu <muxzuo@outlook.com>
2022-08-16 19:52:37 +0800

    code 1656

---
+++ code1656/solution.hpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class OrderedStream {
+public:
+    OrderedStream(int n) {
+        stream.resize(n + 1);
+        ptr = 1;
+    }
+
+    vector<string> insert(int idKey, string value) {
+        stream[idKey] = value;
+        vector<string> res;
+        while (ptr < stream.size() && !stream[ptr].empty()) {
+            res.push_back(stream[ptr]);
+            ++ptr;
+        }
+        return res;
+    }
+
+private:
+    vector<string> stream;
+    int ptr;
+};

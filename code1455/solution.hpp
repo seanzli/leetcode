78812e734f64203a88c40ed0068d335195ea6827
mu <muxzuo@outlook.com>
2022-08-21 08:16:03 +0800

    code 1455

--- code1455/solution.hpp
+++ code1455/solution.hpp
@@ -7,13 +7,41 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    int isPrefixOfWord(string sentence, string searchWord) {
+//        istringstream ss(sentence);
+//        string str;
+//        for (int i = 1; ss >> str; i ++)
+//            if (str.find(searchWord) == 0) return i;
+//        return -1;
+//    }
+//};
+
 class Solution {
+    bool isPrefix(const string& sentence, int& i, const string& searchWord) {
+        int idx = 0;
+        for (; i < sentence.size() && idx < searchWord.size(); ++i, ++idx) {
+            if (sentence[i] == ' ' || sentence[i] != searchWord[idx])
+                return false;
+        }
+        return true;
+    }
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
-        istringstream ss(sentence);
-        string str;
-        for (int i = 1; ss >> str; i ++)
-            if (str.find(searchWord) == 0) return i;
+        int res = 1;
+        for (int i = 0; i < sentence.size(); ++i) {
+            if (sentence[i] == searchWord[0] && isPrefix(sentence, i, searchWord))
+                return res;
+
+
+            for (; i < sentence.size(); ++i) {
+                if (sentence[i] == ' ') {
+                    ++res;
+                    break;
+                }
+            }
+        }
         return -1;
     }
 };

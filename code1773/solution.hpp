d8f7eb4e172c06cf72db58116a5ef645058e53eb
nick <muxzuo@outlook.com>
2022-10-29 10:47:34 +0800

    code 1773

---
+++ code1773/solution.hpp
@@ -0,0 +1,19 @@
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        unordered_map<string, int> dictionary = {{"type", 0}, {"color", 1}, {"name", 2}};
+        int res = 0, index = dictionary[ruleKey];
+        for (auto &&item : items) {
+            if (item[index] == ruleValue) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
